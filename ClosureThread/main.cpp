52c9867e76c249c2a1aac32f2fd9a643725bdfc4
Eric Kampman <erickampman@yahoo.com>
2021-05-26 08:17:21 -0700

    Initial Commit

---
+++ ClosureThread/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ClosureThread
+//
+//  Created by Eric Kampman on 5/26/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
