bc5a131ecec3f3d6a5c6b6f5b72382751f2b3ab0
Eric Kampman <erickampman@yahoo.com>
2021-05-26 13:11:36 -0700

    Added inline closures to lt2 (thread 2).

--- ClosureThread/main.cpp
+++ ClosureThread/main.cpp
@@ -12,14 +12,24 @@
 int main(int argc, const char * argv[]) {
 	
 	std::function<void(TLambdaData<int>)> runf = [](TLambdaData<int> data){
-		std::cout << "run" << std::endl;
+		std::cout << "runf, data: " << data.data_ << std::endl;
 	};
 	std::function<void(TLambdaData<int>)> compf = [](TLambdaData<int> data){
-		std::cout << "completion" << std::endl;
+		std::cout << "compf, data: " << data.data_ << std::endl;
 	};
 	int data = 0;
 	LambdaThread<int> lt(runf, compf, data);
 	lt.run();
 	
+	data = 1;
+	std::cout << "--------------------------------------------------------------------------------" << std::endl;
+	LambdaThread<int> lt2([](TLambdaData<int> data){
+			std::cout << "run lt2, data: " << data.data_ << std::endl;
+		},
+		[](TLambdaData<int> data){
+			std::cout << "completion lt2, data: " << data.data_ << std::endl;
+		},
+		data);
+	lt2.run();
 	return 0;
 }
